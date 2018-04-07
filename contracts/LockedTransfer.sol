f904790bb334d93003ab7fd19d5fbc9bf2030963
Maxi Dev <maxi.canellas@gmail.com>
2018-04-07 18:00:21 -0300

    fixes

--- contracts/LockedTransfer.sol
+++ contracts/LockedTransfer.sol
@@ -6,21 +6,22 @@ import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 contract LockedTransfer is Crowdsale, Ownable {
   
   bool public isLocked;
+  uint256 public unlockTime;
   event unlocked(bool);
 
-  function LockedTransfer() public {
+  function LockedTransfer(uint256 _unlockTime) public {
     isLocked = true;
+    unlockTime = _unlockTime;
   }
    /**
     Function called by the owner to unlock token transfer for contributors.
     */
-  function manualUnlock() public onlyOwner returns (bool) {
+  function unlockTransfer() public onlyOwner returns (bool) {
+    require(block.timestamp >= unlockTime);
     isLocked = false;
     emit unlocked(true);
+    return true;
   } 
 
-  function timeUnlock() public onlyOwner {
-     
-  }
 
 }
